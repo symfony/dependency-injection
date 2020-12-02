dd10fb2e53147655a54c9b7a751d2c20b7051d56
Patrick Zeinert <CoeusCC@users.noreply.github.com>
2020-12-02 12:22:18 -0500

    Add typehints to include() method signature
    Aligns method signature with BaseFileLoader which is the alias of the Symfony\Component\Config\Loader\FileLoader class.

--- Loader/FileLoader.php
+++ Loader/FileLoader.php
@@ -50,7 +50,7 @@ abstract class FileLoader extends BaseFileLoader
      *
      * @param bool|string $ignoreErrors Whether errors should be ignored; pass "not_found" to ignore only when the loaded resource is not found
      */
-    public function import($resource, $type = null, $ignoreErrors = false, $sourceResource = null, $exclude = null)
+    public function import($resource, string $type = null, bool $ignoreErrors = false, string $sourceResource = null, $exclude = null)
     {
         $args = \func_get_args();
 
