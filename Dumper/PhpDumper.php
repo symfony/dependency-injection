377f51513c737c4afc4bc36e8d42f024d5d9da50
Nicolas Grekas <nicolas.grekas@gmail.com>
2025-11-19 09:58:47 +0100

    [DependencyInjection] Fix preloading LazyClosure

--- Dumper/PhpDumper.php
+++ Dumper/PhpDumper.php
@@ -1208,6 +1208,7 @@ EOTXT
                 || ($callable[0] instanceof Definition && !$this->definitionVariables->offsetExists($callable[0]))
             ))) {
                 $initializer = 'fn () => '.$this->dumpValue($callable[0]);
+                $this->preload[LazyClosure::class] = LazyClosure::class;
 
                 return $return.LazyClosure::getCode($initializer, $callable, $class, $this->container, $id).$tail;
             }
